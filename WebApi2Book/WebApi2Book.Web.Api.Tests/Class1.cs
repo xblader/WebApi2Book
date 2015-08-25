5efcfd2eff47b6e195746c96b65fc19b67dd34d2
Victor Hugo <xblader@gmail.com>
2015-08-25 10:46:57 -0300

    Stub project for webapi2

---
+++ WebApi2Book/WebApi2Book.Web.Api.Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi2Book.Web.Api.Tests
+{
+    public class Class1
+    {
+    }
+}
