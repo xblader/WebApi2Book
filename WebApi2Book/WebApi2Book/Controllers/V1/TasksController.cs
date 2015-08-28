76c464e3d99459a4722c00599543b90943fedf68
Victor Hugo <xblader@gmail.com>
2015-08-28 19:54:17 -0300

    com exceção de usuario

--- WebApi2Book/WebApi2Book/Controllers/V1/TasksController.cs
+++ WebApi2Book/WebApi2Book/Controllers/V1/TasksController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebApi2Book.Web.Api.MaintenanceProcessing;
 using WebApi2Book.Web.Api.Models;
 using WebApi2Book.Web.Common;
 using WebApi2Book.Web.Common.Routing;
@@ -14,14 +15,18 @@ namespace WebApi2Book.Web.Api.Controllers.V1
     [UnitOfWorkActionFilter]
     public class TasksController : ApiController
     {
+        private readonly IAddTaskMaintenanceProcessor _addTaskMaintenanceProcessor;
+        public TasksController(IAddTaskMaintenanceProcessor addTaskMaintenanceProcessor)
+        {
+            _addTaskMaintenanceProcessor = addTaskMaintenanceProcessor;
+        }
+
         [Route("", Name = "AddTaskRoute")]
         [HttpPost]
         public Task AddTask(HttpRequestMessage requestMessage, NewTask newTask)
         {
-            return new Task
-            {
-                Subject = "In v1, newTask.Subject = " + newTask.Subject
-            };
+            var task = _addTaskMaintenanceProcessor.AddTask(newTask);
+            return task;
         }
     }
 }
